c833d3d3d4a3f44ad5bb9cc861f5c05a1987a22c
OmarBello <omarbello28@hotmail.com>
2021-02-02 16:47:16 -0400

    Frontend Nuevo

--- Seguimiento a Incidencias/js/scriptIncidencias.js
+++ Seguimiento a Incidencias/js/scriptIncidencias.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-   $('select.Incidencias').change(function(){
-       
+   $("#btnVisualizar").click(function(e){
+      if('#Incidencias == ADMINISTRACION Y FINANZAS' && '#chIncidencias == #flexRadioDefault1'){
+         $.getJSON("https://localhost:44372/api/AdministracionFinanzas/AdministracionFinanzas",function(data){
+            console.log(data);
+            var items = [];
+            for (var i = 0; i < data.sp.length; i++){
+               items.push(data.sp[i]);
+            }
+            var incidencia_data = '';
+            $.each(items, function(key, value){
+               incidencia_data += '<tr>';
+               incidencia_data += '<td>' + value.codigo + +value.proyecto+'</td><br>'; 
+               incidencia_data += '</tr>';
+            });
+            $.each(items, function(key, value){
+               incidencia_data += '<tr>';
+               incidencia_data += '<td>' + value.categoria +'</td>'; 
+               incidencia_data += '<td>' + value.riesgo +'</td>'; 
+               incidencia_data += '</tr>';
+            });
+            $('#Principal').append(incidencia_data);
+            $('#Secundario').append(incidencia_data);
+         });
+      }
    });
 });
